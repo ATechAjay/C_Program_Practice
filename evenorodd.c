673235248b93bcf1997b9b447376fc2cad828e3a
Ajay Yadav <ajayky8298@gmail.com>
2021-03-24 13:52:58 +0530

    Sencond time commit for testing

--- evenorodd.c
+++ evenorodd.c
@@ -2,18 +2,18 @@
 #include<conio.h>
 int main(void)
 {
-	int num;
+	int a;
 	printf("Enter a number::   ");
-	scanf("%d",&num);
-	if(num%2==0)
+	scanf("%d",&a);
+	if(a%2==0)
 	{
 	printf("number is even\n");
 	}
 	else
 	  {
 		printf("Number is odd\n");
-		num++;
-		printf("Next even number is %d \n",num);
+		a++;
+		printf("Next even number is %d \n",a);
   	  }
 		getch();
 }
